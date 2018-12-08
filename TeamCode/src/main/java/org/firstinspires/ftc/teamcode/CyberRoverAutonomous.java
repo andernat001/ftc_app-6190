edd3a589d6239ce7b2164c831f2977a6060a5ee6
ultragamer1010 <31806444+ultragamer1010@users.noreply.github.com>
2018-12-08 00:18:47 -0500

    Add framework of program Add code to: unlock lift, drop robot, strafe to left

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CyberRoverAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CyberRoverAutonomous.java
@@ -5,80 +5,111 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.external.ClassFactory;
-import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
-import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
-import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
-import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
-
-import static java.lang.Thread.sleep;
-
 @Autonomous(name = "Rover", group = "RiderModes")
 public class CyberRoverAutonomous extends CyberRoverAbstract{
 
-    private ElapsedTime timer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);  //Added so opMode does not sleep
+    private ElapsedTime timer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);  // Added so
+    // opMode does not sleep
 
-    //OpenGLMatrix lastLocation = null;
+        //------------------------------------------------------------------
+        // Robot OpMode Loop Method
+        //------------------------------------------------------------------
 
+        @Override
+        public void init() {
 
-    //VuforiaLocalizer vuforia;
-    //BNO055IMU imu;
-    @Override
-    public void init() {
-
-        super.init();
-
-    }
-
-    //Added start to reset encoders when Play is pressed
-    @Override
-    public void start() {
-        motorRightA.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motorRightB.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motorLeftA.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motorLeftB.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-
-    }
-
-    @Override
-    public void loop() {
-
-        switch (seqRobot) {
-
-            case 1:
-
-                seqRobot++;
-                break;
-
-
-            default:
-
-                break;
+            super.init();
 
         }
-    }
 
-    @Override
-    public void stop() {
-        motorLeftA.setPower(0);
-        motorLeftB.setPower(0);
-        motorRightA.setPower(0);
-        motorRightB.setPower(0);
-    }
+        @Override
+        public void loop() {
 
-    private void slp(int slptime) {
-        try {
-            sleep(slptime);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            super.loop();
+
+            // START ROBOT SEQUENCE
+            // Establish the robot sequence of operation with the Switch operation.
+            // The active Case (i.e., sequence step) is established by the value in seqRobot.
+            // After a Case executes, Break the switch to prevent executing subsequent cases unintentionally.
+            switch (seqRobot) {
+                case 1: { // Set all motor power to 0
+                    motorLift.setPower(0);
+                    motorRightA.setPower(0);
+                    motorRightB.setPower(0);
+                    motorLeftA.setPower(0);
+                    motorLeftB.setPower(0);
+
+                    seqRobot++;
+                    timer.reset();
+                    break;
+                }
+
+                case 2: // Transition lift motor to Run-to-Position mode.
+                {
+                    motorLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+                    seqRobot++;
+                    timer.reset();
+                    break;  // Note: The Break command will ensure one scan of code executed while motor
+                    // modes take effect. If Run-to-Position not enabled, unexpected operations
+                    // may occur.
+                }
+
+                case 3: { // Unlock the locking mechanism and set down robot
+                    servoLock.setPosition(SERVO_UNLOCKED);
+                    motorLift.setTargetPosition(LIFT_UP);
+                    motorLift.setPower(0.075);
+
+                    seqRobot++;
+                    timer.reset();
+                    break;
+                }
+
+                case 4: { // Check the position of the motor and don't move on until it is within
+                          // the correct range
+                    if (motorLift.getCurrentPosition() >= LIFT_UP - 10 &&
+                            motorLift.getCurrentPosition() <= LIFT_UP) {
+                        seqRobot++;
+                        timer.reset();
+                        break;
+                    } else {
+                        seqRobot = 4;
+                        timer.reset();
+                        break;
+                    }
+
+                }
+
+                case 5: { // Strafe the robot to the left and off the shuttle
+                    if (timer.milliseconds() < 1500) {
+                        motorLeftA.setPower(0.1);
+                        motorLeftB.setPower(-0.09);
+                        motorRightA.setPower(-0.09);
+                        motorRightB.setPower(0.1);
+                    } else {
+                        motorLeftA.setPower(0);
+                        motorLeftB.setPower(0);
+                        motorRightA.setPower(0);
+                        motorRightB.setPower(0);
+                    }
+                }
+                case 99:  // Done
+                {
+                    break;
+                }
+
+                default: {
+                    break;
+                }
+                // NOTE - By stringing many cases together a complex operation can be built.
+
+            }    // End Robot Sequence
+
+            telemetry.addData("Lift Encoder: ", motorLift.getCurrentPosition());
+            telemetry.addData("Case: ", seqRobot);
+            telemetry.update();
+
+
         }
-    }
-}
+
+    } // End OpMode
